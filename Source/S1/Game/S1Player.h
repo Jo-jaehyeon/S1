6666a05c697bd131d5ab6cca5ceb596de5d56a49
Jo-Jaehyeon <jerryjo0906@gmail.com>
2025-02-20 12:34:03 +0900

    이동 동기화 보정 처리

--- Source/S1/Game/S1Player.h
+++ Source/S1/Game/S1Player.h
@@ -22,12 +22,16 @@ protected:
 
 public:
 	bool IsMyPlayer();
+	Protocol::MoveState GetMoveState() { return PlayerInfo->state(); }
+	void SetMoveState(Protocol::MoveState State);
 
 public:
 	void SetPlayerInfo(const Protocol::PlayerInfo& Info);
+	void SetDestInfo(const Protocol::PlayerInfo& Info);
 	Protocol::PlayerInfo* GetPlayerInfo() { return PlayerInfo; }
 
 protected:
 	class Protocol::PlayerInfo* PlayerInfo; //현재 위치
+	class Protocol::PlayerInfo* DestInfo;	//목적지 위치
 };
 
